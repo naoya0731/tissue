5ffedbf01ef95487064ae289eb2764d0d63013a2
Naoya Fujita <wfknaoya0731@gmail.com>
2018-07-08 00:49:29 +0900

    new

---
+++ app/models/post.rb
@@ -0,0 +1,20 @@
+class Post < ApplicationRecord
+  extend FriendlyId
+  friendly_id :key, use: :slugged
+  validates :title, presence: true
+  validates :body, presence: true
+
+  def self.new_key
+    is_new = false
+    while !is_new
+      key = SecureRandom.hex(10)
+      is_new = true if self.where(key: key).size.zero?
+    end
+    key
+  end
+
+  def url
+    host = Rails.env.development? ? "http://localhost:3000" : ""
+    url = "#{host}/posts/#{self.key}"
+  end
+end
