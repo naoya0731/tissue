fe31f32f8919f611e7898e21ca5771c04370e741
Naoya Fujita <wfknaoya0731@gmail.com>
2018-07-08 15:20:35 +0900

    text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,24 @@ module ApplicationHelper
   def qrcode_tag(text, options = {})
     ::RQRCode::QRCode.new(text).as_svg(options).html_safe
   end
+
+  def default_meta_tags
+    {
+      site: 'ティッシュ',
+      reverse: true,
+      title: 'コンテンツをすぐ共有！',
+      description: 'ティッシュはコンテンツをすぐ共有するためのサービスです。',
+      keywords: '共有',
+      canonical: request.original_url,
+      og: {
+        title: :title,
+        type: :page,
+        url: request.original_url,
+        #image: image_url(Settings.site.meta.ogp.image_path),
+        site_name: 'ティッシュ',
+        description: :description,
+        locale: 'ja_JP'
+      }
+    }
+  end
 end
